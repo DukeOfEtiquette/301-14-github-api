5b54ef4d9542dbed4af690ba121c13977283e597
Sam Hamm <sam@codefellows.com>
2018-06-22 10:49:31 -0700

    End of class

--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
+
+$.get('/github/user/repos').then(data => {
+  // console.log(data)
+  JSON.parse(data).forEach(repo =>
+    $('#results').append(`<p>${repo.name}</p>`));
+})
--- server.js
+++ server.js
@@ -8,9 +8,16 @@ app.use(express.static('./public'));
 app.use(cors());
 
 // Here is where we will manage out GitHub requests
-// app.get('/github/*', (req, res) => {
-//   console.log('Routing a request to GitHub');
-// });
+app.get('/github/*', (req, res) => {
+  console.log('Routing a GitHub AJAX request for ', req.params[0]);
+  const url = `https://api.github.com/${req.params[0]}`;
+  superagent.get(url)
+    .set(`Authorization`, `token ${process.env.GITHUB_TOKEN}`)
+    .then(
+      repos => res.send(repos.text),
+      err => res.send(err)
+    )
+})
 
 app.get('/', (req, res) => {
   res.sendFile('index.html')

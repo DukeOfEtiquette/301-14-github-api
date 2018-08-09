e12adf3169528b41119b2e0031c9e5bccbbaaf25
DukeOfEtiquette <duquetteadam@gmail.com>
2018-08-09 14:29:31 -0700

    init commit

--- public/app.js
+++ public/app.js
@@ -1,7 +1,19 @@
 // Let's make an AJAX call to the GitHub API and then do a simple render of the data into the DOM
 
-$.get('/github/user/repos').then(data => {
-  // console.log(data)
-  JSON.parse(data).forEach(repo =>
-    $('#results').append(`<p>${repo.name}</p>`));
-})
+function appendError(err){
+  $('#results').append('<h1 style="color: red;">ERROR</h1>');
+  appendData(err);
+}
+
+function appendData(data){
+  for(var key in data){
+    $('#results').append(`<p><strong>${key}</strong>: ${data[key]}</p>`);
+  }
+}
+
+$(function(){
+  let url = 'https://api.github.com/users/dukeofetiquette';
+  let url2 = 'https://api.github.com/user';
+
+
+});
